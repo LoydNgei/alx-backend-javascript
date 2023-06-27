c84659f5a5283368a8a3bf01362fa2a8e03ffe62
loyd ngei <loydngei@gmail.com>
2023-06-27 18:05:57 +0300

    11 and 12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,14 @@
+// import createEmployeesObject from './11-createEmployeesObject';
+
+export default function createReportObject(employeesList) {
+  const reportObject = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments() {
+      return Object.keys(this.allEmployees).length;
+    },
+  };
+
+  return reportObject;
+}
