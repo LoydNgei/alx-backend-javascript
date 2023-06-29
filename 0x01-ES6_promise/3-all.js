3b55366a5a6797a4f28d10010911246219a262c9
loyd ngei <loydngei@gmail.com>
2023-06-29 15:52:48 +0300

    Eslint

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,14 +1,15 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-return Promise.all([uploadPhoto(), createUser()])
+  const A = uploadPhoto();
+  const B = createUser();
+  return Promise.all([A, B])
     .then((data) => {
-        const [Photo, User] = data;
-        console.log(`${Photo.body} ${User.firstName} ${User.lastName}`);
+      console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`);
     })
-    .catch((error) => {
-        console.error('Signup system offline');
+    .catch(() => {
+      console.log('Signup system offline');
     });
 }
 
-//export default handleProfileSignup();
+// export default handleProfileSignup();
