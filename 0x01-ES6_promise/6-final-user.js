ff702147e1039efb905f08d6c471d667c95e1807
loyd ngei <loydngei@gmail.com>
2023-07-03 15:59:14 +0300

    Async

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   const promises = [
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
@@ -9,22 +9,20 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
   const feedback = [];
 
-  return Promise.allSettled(promises)
-    .then((results) => {
-      results.forEach((result) => {
-        const { status } = result;
-        const value = status === 'fulfilled' ? result.value : result.reason.message;
+  try {
+    const results = await Promise.allSettled(promises);
+    results.forEach((result) => {
+      const { status } = result;
+      const value = status === 'fulfilled' ? result.value : result.reason.message;
 
-        feedback.push({
-          status,
-          value,
-        });
+      feedback.push({
+        status,
+        value,
       });
-
-      return feedback;
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      return feedback;
     });
+    return feedback;
+  } catch (error) {
+    console.error('Error:', error);
+    return feedback;
+  }
 }
