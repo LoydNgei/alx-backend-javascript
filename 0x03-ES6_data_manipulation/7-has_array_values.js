650c0137c93df81146983eb8c2f61984d81a3a31
loyd ngei <loydngei@gmail.com>
2023-07-07 09:19:52 +0300

    Has set

--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,10 +1,14 @@
+// For us to check if all elements in the array exist within the set,
+// We have to loop through the array first and apply a condition to
+// check for common elements in both
+
 function hasValuesFromArray(set, array) {
-    for (const element of array) {
-        if (!set.has(element)) {
-            return false;
-        }
+  for (const element of array) {
+    if (!set.has(element)) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 export default hasValuesFromArray;
