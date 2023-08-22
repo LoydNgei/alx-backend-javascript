0f2ba4d573239beab92948ea0fea2f3c1e7c7662
Loyd Ngei <loydngei@gmail.com>
2023-08-22 23:56:27 +0300

    NodeJS

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,23 @@
+// 1-stdin.js
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (input) => {
+  if (input.trim() === '') {
+    rl.close();
+    return;
+  }
+
+  console.log(`Your name is: ${input}`);
+});
+
+rl.on('close', () => {
+  console.log('This important software is now closing');
+});
---
+++ 0x05-Node_JS_basic/README.md
@@ -0,0 +1,1 @@
+Working on NodeJS project today. Dealing with a bit of ExpressJS too
