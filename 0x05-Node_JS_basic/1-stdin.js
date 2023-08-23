7aef20ff17cb695837b3ba0338ecedd580d53896
Loyd Ngei <loydngei@gmail.com>
2023-08-23 09:33:07 +0300

    Stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,13 +3,13 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
     const input = data.toString();
-    process.stdout.write(`Your name is: ${input}\n`);
+    process.stdout.write(`Your name is: ${input}`);
     process.exit();
   });
 } else {
   process.stdin.on('data', (data) => {
     const input = data.toString();
-    process.stdout.write(`Your name is: ${input}\n`);
+    process.stdout.write(`Your name is: ${input}`);
     process.exit();
   });
   process.on('exit', () => {
