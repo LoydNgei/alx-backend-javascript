9f630ec4a21fe5395b1d6284176adbff1b284b2d
Loyd Ngei <loydngei@gmail.com>
2023-08-25 10:11:58 +0300

    Calculation test

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,9 +3,9 @@ const calculateNumber = require('./0-calcul');
 
 describe('Calculation test', function() {
     it('checks if RoundOf works perfectly', function() {
-        assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-        assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+        assert.equal(calculateNumber(1, 3), 4);
+        assert.equal(calculateNumber(1, 3.7), 5);
+        assert.equal(calculateNumber(1.2, 3.7), 5);
+        assert.equal(calculateNumber(1.5, 3.7), 6);
     });
 });
