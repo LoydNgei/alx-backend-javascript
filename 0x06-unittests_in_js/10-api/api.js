69d95a91465607ab749d6aeb514ec7a2bcd4c82f
Loyd Ngei <loydngei@gmail.com>
2023-08-26 01:08:51 +0300

    API

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -21,7 +21,7 @@ app.get('/available_payments', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-    const username = req.body.username;
+    const username = req.body.userName;
     res.end(`Welcome ${username}`);
 });
 
